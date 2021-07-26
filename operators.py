82a15a1b9375f79dbbbec6539d56da85fcfffd19
Jeyaprakash-dr <69540530+Jeyaprakash-dr@users.noreply.github.com>
2021-07-26 11:31:19 -0500

    third connut

---
+++ operators.py
@@ -0,0 +1,5 @@
+# arithmetic opeartions
+num1 = 10
+num2 = 124
+solution = num1 + num2
+print("Soultion = " + str(solution))
--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
-print("hello world!")
+# this is a single-line comment
+
+print("code that is executed")
+
+"""
+    this is a comment that
+    spans multiple lines
+"""
+print("code that is executed next")
+num = 1
+username = input("Enter you name: ")
+#money = 9.50
+money = float(input("How much do you have: "))
+money = money + num
+print("hello " + username + "! You have: $" + str(money))
+
+is_raining = False # boolean data type
